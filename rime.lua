f69f6d793bbafb023726ba0128b72f62f726bcef
Lufsx <Lufs00@outlook.com>
2021-04-23 21:23:35 +0800

    feat: add dynamic week

--- rime.lua
+++ rime.lua
@@ -2,6 +2,7 @@ function date_translator(input, seg)
     if (input == "date") then
         --- Candidate(type, start, end, text, comment)
         yield(Candidate("date", seg.start, seg._end, os.date("%Y-%m-%d"), "Date"))
+        yield(Candidate("date", seg.start, seg._end, os.date("%Y年%m月%d日"), ""))
         yield(Candidate("date", seg.start, seg._end, os.date("%Y%m%d"), ""))
     end
     if (input == "time") then
@@ -9,9 +10,14 @@ function date_translator(input, seg)
         yield(Candidate("date", seg.start, seg._end, os.date("%H:%M:%S"), "Time"))
         yield(Candidate("time", seg.start, seg._end, os.date("%H%M%S"), ""))
     end
-    if (input == "riqi") then
-        --- Candidate(type, start, end, text, comment)
-        yield(Candidate("date", seg.start, seg._end, os.date("%Y年%m月%d日"), "日期"))
+
+    -- @JiandanDream
+    -- https://github.com/KyleBing/rime-wubi86-jidian/issues/54
+
+    if (input == "week") then
+        local weakTab = {'日', '一', '二', '三', '四', '五', '六'}
+        yield(Candidate("week", seg.start, seg._end, "周" .. weakTab[tonumber(os.date("%w") + 1)], ""))
+        yield(Candidate("week", seg.start, seg._end, "星期" .. weakTab[tonumber(os.date("%w") + 1)], ""))
     end
 end
 
