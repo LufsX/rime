2f8bead6e70a7904c502634b23db3313c82e4bcc
LufsX <lufsx@outlook.com>
2023-01-14 00:28:18 +0800

    feat: add new filters
    正确调用过滤器，并添加两个类型
    最长词组和单字在先 与 只显示单字

--- rime.lua
+++ rime.lua
@@ -1,5 +1,34 @@
+--- 过滤器：最长词组和单字在先
+function long_phrase_first(input)
+    local l = {}
+    local s = {}
+    local c = {}
+    local max = 1
+    for cand in input:iter() do
+        if (utf8.len(cand.text) > max) then
+            max = utf8.len(cand.text)
+        end
+        if (utf8.len(cand.text) == 1) then
+            table.insert(c, cand)
+        elseif (utf8.len(cand.text) < max) then
+            table.insert(s, cand)
+        else
+            table.insert(l, cand)
+        end
+    end
+    for i, cand in ipairs(l) do
+        yield(cand)
+    end
+    for i, cand in ipairs(c) do
+        yield(cand)
+    end
+    for i, cand in ipairs(s) do
+        yield(cand)
+    end
+end
+
 --- 过滤器：单字在先
-function single_char_first_filter(input)
+function single_char_first(input)
     local l = {}
     for cand in input:iter() do
         if (utf8.len(cand.text) == 1) then
@@ -10,6 +39,15 @@ function single_char_first_filter(input)
     end
     for i, cand in ipairs(l) do
         yield(cand)
+    end
+end
+
+--- 过滤器：只显示单字
+function single_char_only(input)
+    for cand in input:iter() do
+        if (utf8.len(cand.text) == 1) then
+            yield(cand)
+        end
     end
 end
 
