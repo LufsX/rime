ca18033551b60c0db68304ed9e5f9b6d8d01fd48
LufsX <lufsx@outlook.com>
2023-05-08 03:06:29 +0800

    chore: compatible with legacy librime-lua
    https://github.com/hchunhui/librime-lua/wiki/Scripting#%E6%97%A7%E7%89%88-librime-lua

---
+++ rime.lua
@@ -0,0 +1,19 @@
+-- select_character_processor: 以词定字
+-- 详见 `lua/select_character.lua`
+select_character = require("select_character")
+
+-- date_translator: 动态日期时间输入
+-- 详见 `lua/date_translator.lua`
+date_translator = require("date_translator")
+
+-- long_phrase_first: 最长词组和单字在先
+-- 详见 `lua/candidate_sorting/long_phrase_first.lua`
+long_phrase_first = require("candidate_sorting.long_phrase_first")
+
+-- single_char_first: 单字在先
+-- 详见 `lua/candidate_sorting/single_char_first.lua`
+single_char_first = require("candidate_sorting.single_char_first")
+
+-- single_char_only: 只显示单字
+-- 详见 `lua/candidate_sorting/single_char_only.lua`
+single_char_only = require("candidate_sorting.single_char_only")
