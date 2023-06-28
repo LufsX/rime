d282426e7a3adb43bebcd1af2a4c19e780fcf49b
LufsX <lufsx@outlook.com>
2023-06-29 01:47:13 +0800

    update: dicts

---
+++ tools/clear.py
@@ -0,0 +1,20 @@
+import os, sys, re
+
+workDir = os.path.abspath(os.path.dirname(sys.path[0]))
+pattern = re.compile(r'^[\u4e00-\u9fa5]+$')
+
+with open(
+    os.path.join(workDir, "opencc", "emoji_word.txt"), "r", encoding="utf-8"
+) as f:
+    text = f.read()
+
+lines = text.split("\n")
+
+with open(
+    os.path.join(workDir, "opencc", "emoji_word.txt"), "w", encoding="utf-8"
+) as outFile:
+    for line in lines:
+        if line:
+            parts = line.split("\t")
+            if pattern.match(parts[0]):
+                outFile.write(line + "\n")
