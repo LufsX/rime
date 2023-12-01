2cb0f661582d8322fc35b0f202d127a13254c1c7
LufsX <lufsx@outlook.com>
2023-12-01 16:50:03 +0800

    chore: fixed sort rule
    and add update tool

---
+++ tools/update.py
@@ -0,0 +1,21 @@
+import requests, os, sys
+
+workDir = os.path.abspath(os.path.dirname(sys.path[0]))
+
+update_url = {
+    os.path.join(
+        workDir, "dicts", "simp.dict.yaml"
+    ): "https://github.com/iDvel/rime-ice/raw/main/cn_dicts/8105.dict.yaml",
+    os.path.join(
+        workDir, "opencc", "emoji_word.txt"
+    ): "https://github.com/iDvel/rime-ice/raw/main/opencc/emoji.txt",
+}
+
+for path, link in update_url.items():
+    try:
+        response = requests.get(link)
+        if response.status_code == 200:
+            with open(path, "wb") as f:
+                f.write(response.content)
+    except Exception as e:
+        print(f"{path} 报错: {str(e)}")
---
+++ tools/util.py
@@ -0,0 +1,133 @@
+def deduplicate(src_file, dest_file):
+    lines_seen = set()
+    output_lines = []
+
+    with open(src_file, "r", encoding="utf-8") as file:
+        for line in file:
+            stripped_line = line.strip()
+            if (
+                stripped_line == ""
+                or stripped_line.startswith("#")
+                or stripped_line not in lines_seen
+            ):
+                output_lines.append(line)
+                if stripped_line != "":
+                    lines_seen.add(stripped_line)
+
+    with open(dest_file, "w", encoding="utf-8") as file:
+        file.writelines(output_lines)
+
+
+def get_info(src_file):
+    with open(src_file, "r", encoding="utf-8") as f:
+        lines = f.readlines()
+
+    info = []
+    flag_continue = False
+
+    for line in lines:
+        if line == "\n" or line.startswith("#"):
+            info.append(line)
+        elif line == "---\n":
+            info.append(line)
+            flag_continue = True
+        elif flag_continue:
+            info.append(line)
+            if line == "...\n":
+                flag_continue = False
+        else:
+            break
+
+    return info
+
+
+def sort(src_file, dest_file):
+    info = get_info(src_file)
+
+    # 跳过处理 THUOCL 词库
+    if "thuocl" in src_file:
+        return
+
+    with open(src_file, "r", encoding="utf-8") as f:
+        lines = f.readlines()[len(info) :]
+
+    format_type = len(lines[0].split("\t"))
+
+    # format_type == 2 时，排序「字	zi」此类，规则是按拼音排序
+    # format_type == 3 时，排序「字	zi	1」此类，规则是按拼音+字频排序
+    # 暂时不支持混排
+    if format_type == 2:
+        wait_to_write = []
+        part_to_sort = []
+
+        for line in lines:
+            if line.startswith("#") or line == "\n":
+                if part_to_sort:
+                    sorted_lines = sorted(
+                        part_to_sort,
+                        key=lambda x: x.split("\t")[1]
+                        if len(x.split("\t")) > 1
+                        else "",
+                    )
+                    wait_to_write += sorted_lines
+                    part_to_sort.clear()
+                wait_to_write.append(line)
+                continue
+            part_to_sort.append(line)
+
+        if part_to_sort:
+            wait_to_write += sorted(
+                part_to_sort,
+                key=lambda x: x.split("\t")[1] if len(x.split("\t")) > 1 else "",
+            )
+        with open(dest_file, "w", encoding="utf-8") as f:
+            f.writelines(info + wait_to_write)
+    elif format_type == 3:
+        wait_to_write = []
+        part_to_sort = []
+
+        for line in lines:
+            if line.startswith("#") or line == "\n":
+                if part_to_sort:
+                    sorted_lines = sorted(
+                        part_to_sort,
+                        key=lambda x: (
+                            x.split("\t")[1] if len(x.split("\t")) > 1 else "",
+                            int(x.split("\t")[2])
+                            if len(x.split("\t")) > 2 and x.split("\t")[2].isdigit()
+                            else 0,
+                        ),
+                    )
+                    wait_to_write += sorted_lines
+                    part_to_sort.clear()
+                wait_to_write.append(line)
+                continue
+            part_to_sort.append(line)
+
+        if part_to_sort:
+            wait_to_write += sorted(
+                part_to_sort,
+                key=lambda x: (
+                    x.split("\t")[1] if len(x.split("\t")) > 1 else "",
+                    int(x.split("\t")[2])
+                    if len(x.split("\t")) > 2 and x.split("\t")[2].isdigit()
+                    else 0,
+                ),
+            )
+        with open(dest_file, "w", encoding="utf-8") as f:
+            f.writelines(info + wait_to_write)
+
+
+if __name__ == "__main__":
+    import os
+    import sys
+
+    dicts_dir = os.path.join(os.path.abspath(os.path.dirname(sys.path[0])), "dicts")
+
+    for root, _, files in os.walk(dicts_dir):
+        for file in files:
+            if file.endswith(".yaml"):
+                file_path = os.path.join(root, file)
+                print(f"Processing {file_path}")
+                deduplicate(file_path, file_path)
+                sort(file_path, file_path)
