3373da0d70f5e1980b56fa2529e8ef7b4abaf1c2
LufsX <lufsx@outlook.com>
2025-04-25 16:31:30 +0800

    feat: opt select_character
    feat: adapt no candidates
    feat: adapt to single character logic
    feat: new disable select_character way

--- lua/select_character.lua
+++ lua/select_character.lua
@@ -1,93 +1,45 @@
 -- select_character: 以词定字
 -- 详见: https://github.com/BlindingDark/rime-lua-select-character
-
--- http://lua-users.org/lists/lua-l/2014-04/msg00590.html
-local function utf8_sub(s, i, j)
-    i = i or 1
-    j = j or -1
-
-    if i < 1 or j < 1 then
-        local n = utf8.len(s)
-        if not n then
-            return nil
-        end
-        if i < 0 then
-            i = n + 1 + i
-        end
-        if j < 0 then
-            j = n + 1 + j
-        end
-        if i < 0 then
-            i = 1
-        elseif i > n then
-            i = n
-        end
-        if j < 0 then
-            j = 1
-        elseif j > n then
-            j = n
-        end
+-- 2025-04-25: 优化单字提取逻辑，处理无候选的情况
+local function extract_character(s, is_first)
+    -- 单字不处理
+    if utf8.len(s) == 1 then
+        return s
     end
-
-    if j < i then
-        return ""
-    end
-
-    i = utf8.offset(s, i)
-    j = utf8.offset(s, j + 1)
-
-    if i and j then
-        return s:sub(i, j - 1)
-    elseif i then
-        return s:sub(i)
-    else
-        return ""
-    end
-end
-
-local function first_character(s)
-    return utf8_sub(s, 1, 1)
-end
-
-local function last_character(s)
-    return utf8_sub(s, -1, -1)
-end
-
-local function get_commit_text(context, fun)
-    local candidate_text = context:get_selected_candidate().text
-    local selected_character = fun(candidate_text)
-
-    context:clear_previous_segment()
-    local commit_text = context:get_commit_text()
-
-    context:clear()
-
-    return commit_text .. selected_character
+    return is_first and s:sub(1, utf8.offset(s, 2) - 1) -- 首字
+    or s:sub(utf8.offset(s, -1)) -- 尾字
 end
 
 local function select_character(key, env)
     local engine = env.engine
     local context = engine.context
     local config = engine.schema.config
+    local candidate = context:get_selected_candidate()
 
-    local first_key = config:get_string('key_binder/select_first_character') or 'bracketleft'
-    local last_key = config:get_string('key_binder/select_last_character') or 'bracketright'
+    -- 获取按键
+    local first_key = config:get_string('key_binder/select_first_character')
+    local last_key = config:get_string('key_binder/select_last_character')
+    local key_repr = key:repr()
 
-    local commit_text = context:get_commit_text()
+    -- 检查按键并给予 is_first 标记
+    local is_first = (key_repr == first_key)
+    if not (is_first or key_repr == last_key) then
+        return 2 -- kNoop
+    end
 
-    if (key:repr() == first_key and commit_text ~= "") then
-        engine:commit_text(get_commit_text(context, first_character))
-
+    -- 如果没有已选的词，直接上屏当前输入
+    if not candidate then
+        engine:commit_text(context:get_commit_text())
+        context:clear()
         return 1 -- kAccepted
     end
 
-    if (key:repr() == last_key and commit_text ~= "") then
-        engine:commit_text(get_commit_text(context, last_character))
-
-        return 1 -- kAccepted
-    end
-
-    return 2 -- kNoop
+    -- 提取候选并提交
+    local selected_char = extract_character(candidate.text, is_first)
+    context:clear_previous_segment()
+    engine:commit_text(context:get_commit_text() .. selected_char)
+    context:clear()
+    return 1 -- kAccepted
 end
 
 return select_character
